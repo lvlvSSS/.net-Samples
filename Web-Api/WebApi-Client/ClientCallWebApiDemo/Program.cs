9a303b64fa88de89cf5bfa95870ebe231b9d6a6d
nelson <lvhaolin007@126.com>
2019-04-25 23:41:11 -0700

    Add the demo for WebApi , the simple server and client

---
+++ Web-Api/WebApi-Client/ClientCallWebApiDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCallWebApiDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person() { age = 11, name = "nelson" };
+            List<Person> liPerosns = new List<Person>();
+            liPerosns.Add(new Person() { age = 11, name = "nelson" });
+            liPerosns.Add(new Person() { age = 11, name = "nelson" });
+            liPerosns.Add(new Person() { age = 11, name = "nelson" });
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.BaseAddress = new Uri(@"http://localhost:2320/");
+
+                //序列化为json字符串。
+                //var stringContent = JsonConvert.SerializeObject(liPerosns);
+                //HttpContent content = new FormUrlEncodedContent(stringContent);
+                //content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+                //发送键值对数据。
+                var temp = new List<KeyValuePair<string, string>>();
+                temp.Add(new KeyValuePair<string, string>("", "Here is the test"));
+                HttpContent content = new FormUrlEncodedContent(temp);
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+                //发送，获取结果。
+                var res = client.PostAsync("specialAPI/values/postwithoutparams", content).Result;
+                Console.WriteLine(res.Content.ReadAsStringAsync().Result);
+                Console.ReadKey();
+            }
+        }
+
+        public class Person
+        {
+            public int age;
+            public string name;
+        }
+    }
+}
---
+++ Web-Api/WebApi-Server/TestDemo/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestDemo.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        [Route("specialAPI/values/postwithoutparams")]
+        public string Post([FromBody]string value /*[FromBody]Person person*/)//在一个api的参数中，FromBody只能出现一次！切记！！！
+        {
+            /*
+             * Request.Content.ReadAsStreamAsync().Result.Seek(0, System.IO.SeekOrigin.Begin);
+             * 解释如下：
+             * 当你的Action参数存在[FromBody]等读取内容的方法时，会被[FromBody]“吃掉”。
+             * 说得有些生动，其实原因是读取完毕后，指向内容的指针会指向最后的结束符，再次读取的时候就读不到了，
+             * 因此解决方法有两种：
+               1、不需要[FromBody]，直接在代码里用上述代码读取原始内容。
+               2、保留[FromBody]，重新调整内容指针，再进行读取。
+             */
+            //var person1 = Request.Content.ReadAsAsync<Person>().Result;
+            Request.Content.ReadAsStreamAsync().Result.Seek(0, System.IO.SeekOrigin.Begin);
+            var re = Request.Content.ReadAsStringAsync().Result;
+            return re ?? "none";
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Person
+    {
+        public int age;
+        public string name;
+    }
+}
