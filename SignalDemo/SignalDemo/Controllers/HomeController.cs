770620847711b599e61ad7f268c14484eef4d5e0
Nelson Lv <Nelson.Lv@bd.com>
2019-05-01 01:57:28 +0800

    Add the signal demo

---
+++ SignalDemo/SignalDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using SignalDemo.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.ClientName = "聊客-" + Guid.NewGuid().ToString("N");
+            var onLineUserList = ChatHub.OnLineUsers.Select(u => new SelectListItem() { Text = u.Value, Value = u.Key }).ToList();
+            onLineUserList.Insert(0, new SelectListItem() { Text = "-所有人-", Value = "" });
+            ViewBag.OnLineUsers = onLineUserList;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SignalDemo/SignalDemo/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalDemo.Startup))]
+namespace SignalDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
